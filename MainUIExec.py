49eb1133ac29afbb1f6bd6d98022b9381226393b
Isralian <isfa98@hotmail.com>
2020-10-25 12:43:19 -0600

    Added User Interface and UI handler. Implemented FileOpener at 50%

---
+++ MainUIExec.py
@@ -0,0 +1,15 @@
+from PySide2 import QtWidgets
+from UI import UIMain
+
+
+class UI(UIMain.Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+    qt_app = UI()
+    qt_app.show()
+    app.exec_()
