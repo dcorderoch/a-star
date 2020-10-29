90e94bfa8390bb9913d0fd06b7fa23cb26a7673d
Isralian <isfa98@hotmail.com>
2020-10-29 00:41:08 -0600

    Changes in UIMain.py -Added Buttons to load initial configuration, Final configuration, resolve, shuffle, play solution, intructions and save configs and steps to solve.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from PySide2.QtWidgets import QMainWindow, QApplication
-from GUI.window import Ui_MainWindow
+from PySide2 import QtWidgets
+from UI import UIMain
 
-class UI(Ui_MainWindow, QMainWindow):
+class UI(UIMain.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
         self.setupUi(self)
 
 if __name__ == '__main__':
-    app = QApplication()
+    app = QtWidgets.QApplication()
     qt_app = UI()
     qt_app.show()
     app.exec_()
