ee69e7c6082ba178098ed86f59e8337c2ef1c77e
Esteban <nano.95.eb@gmail.com>
2020-10-31 18:18:50 -0600

    loading initial configuration

--- src/game.py
+++ src/game.py
@@ -1,3 +1,4 @@
+from enum import Enum
 
 
 class BoardTokens(Enum):
@@ -11,41 +12,101 @@ class BoardTokens(Enum):
 
 class Game:
     # el primer row del tablero solo puede contener una pieza, por lo que efectivamente el tablero empieza en el 2ndo row
-    self._board = [[BoardTokens.NOT_USED, BoardTokens.NOT_USED, BoardTokens.NOT_USED, BoardTokens.FREE_SPACE],
-                   [BoardTokens.TOKEN1, BoardTokens.TOKEN2,
-                       BoardTokens.TOKEN3, BoardTokens.TOKEN4],
-                   [BoardTokens.TOKEN1, BoardTokens.TOKEN2,
-                       BoardTokens.TOKEN3, BoardTokens.TOKEN4],
-                   [BoardTokens.TOKEN1, BoardTokens.TOKEN2,
-                       BoardTokens.TOKEN3, BoardTokens.TOKEN4],
-                   [BoardTokens.TOKEN1, BoardTokens.TOKEN2, BoardTokens.TOKEN3, BoardTokens.TOKEN4]]
-    self._final_board = self._board
+    _board = [[BoardTokens.NOT_USED, BoardTokens.NOT_USED, BoardTokens.NOT_USED, BoardTokens.FREE_SPACE],
+              [BoardTokens.TOKEN1, BoardTokens.TOKEN2,
+               BoardTokens.TOKEN3, BoardTokens.TOKEN4],
+              [BoardTokens.TOKEN1, BoardTokens.TOKEN2,
+               BoardTokens.TOKEN3, BoardTokens.TOKEN4],
+              [BoardTokens.TOKEN1, BoardTokens.TOKEN2,
+               BoardTokens.TOKEN3, BoardTokens.TOKEN4],
+              [BoardTokens.TOKEN1, BoardTokens.TOKEN2, BoardTokens.TOKEN3, BoardTokens.TOKEN4]]
+    _final_board = _board
+
+    redBackground = "rgb(255, 103, 103)"
+    greenBackground = "rgb(0, 170, 127)"
+    blueBackground = "rgb(88, 155, 255)"
+    yellowBackground = "rgb(255, 255, 127)"
 
     # se encarga de cargar la configuración de un archivo .txt, carga configuración final e inicial (crear otra si es necesario)
-    def loadConfig():
-        return
+    def loadConfig(self, config, ui):
+        row = 0
+        if self.validate_configuration(config) == False:
+            print("Error: There are not the required number of colors in the suplied configuration. There must be exactly 4 occurrences of each color")
+            return
 
+        for line in config:
+            if row >= 4:
+                print("Warning: There are more rows than needed, skipping...")
+                break
+
+            trimedLine = line.replace('\n', '')
+            trimedLine = trimedLine.replace(' ', '')
+
+            elements = trimedLine.split(',')
+            column = 0
+            for element in elements:
+                if column >= 4:
+                    print("Warning: There are more columns than needed, skipping...")
+                    break
+                if not element.isdigit() or 0 <= int(element) >= 4:
+                    print("Error: Invalid value: \"" + element +
+                          "\". Cannot load configuration.")
+                    self.reset_colors(ui)
+                    return
+                button = ui.layout[row][column]
+                button.setStyleSheet(self.getColor(element))
+                column += 1
+            row += 1
+
+    def reset_colors(self, ui):
+        for column in range(0, 4):
+            for row in range(0, 4):
+                button = ui.layout[row][column]
+                button.setStyleSheet(self.getColor(str(column)))
+
+    def validate_configuration(self, configuration):
+        singleConfig = ','.join(configuration)
+        if singleConfig.count("0") != 4:
+            return False
+        if singleConfig.count("1") != 4:
+            return False
+        if singleConfig.count("2") != 4:
+            return False
+        if singleConfig.count("3") != 4:
+            return False
+        return True
+
+    def getColor(self, colorCode):
+        if colorCode == "0":
+            return u"background-color: " + self.redBackground + ";"
+        elif colorCode == "1":
+            return u"background-color: " + self.greenBackground + ";"
+        elif colorCode == "2":
+            return u"background-color: " + self.blueBackground + ";"
+        elif colorCode == "3":
+            return u"background-color: " + self.yellowBackground + ";"
     # se encarga de guardar la configuración en un archivo de .txt
-    def saveConfig():
+
+    def saveConfig(self):
         return
 
     # se encarga de revolver el tablero de juego
-    def shuffle():
+    def shuffle(self):
         return
 
     # se encarga de ejecutar A* para buscar la solución partiendo de _board y llegando a _final_board.
     # Debe guarda los pasos de solución de algún modo que pueda ser luego ejecutado
-    def solvePuzzle():
+    def solvePuzzle(self):
         return
 
     # se encarga de rotar los valores del _board en la fila(row) seleccionada por la cantidad en rotation, en la direccion (0=izq, 1=derecha)
-    def rotateRow(row, direction, rotation=1):
+    def rotateRow(self, row, direction, rotation=1):
         return
 
     # mueve la posición vacía al lugar indicado si esta se encuentra en ese row o esa col
-    def moveEmptySpace(row, col):
+    def moveEmptySpace(self, row, col):
         return
 
     # Se ejecutan los movimientos de la solución uno a uno (PROBABLEMENTE VA EN LA CLASE DE UI!!!)
-    def playSolution():
+    def playSolution(self):
         return
--- src/main.py
+++ src/main.py
@@ -1,10 +1,41 @@
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
 from PySide2 import QtWidgets
 from UI import UIMain
+
+from game import *
+
 
 class UI(UIMain.Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self):
         super(UI, self).__init__()
+        self.game = Game()
         self.setupUi(self)
+        self.set_button_handlers()
+
+    def set_button_handlers(self):
+        self.btnLoadInitialConfig.clicked.connect(self.btnLoadFileHandler)
+
+    # Handler of the Load Button, to calls the file opener
+    def btnLoadFileHandler(self):
+        print("Button pressed")
+        self.openFileOpener()
+    # btnLoadFileHandler
+
+    # File opener function, to open and read the .txt with the configuration
+    def openFileOpener(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+
+        if path != '':
+            print('file:', path)
+            with open(path, "r") as config:
+                initialConfig = config.readlines()
+                self.game.loadConfig(initialConfig, self)
+    # openFileOpener
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication()
